23394bc09d8dd3d01f8dc0fd3700bdb4c91fce6a
Maurice Nicholson <maurice@mauricenicholson.com>
2023-12-11 10:04:45 +0000

    working part2 solution thanks to @hyper-neutrino

--- day-12/src/bin/part2.rs
+++ day-12/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 fn main() {
@@ -9,51 +11,7 @@ fn main() {
 #[derive(Debug)]
 struct Record {
     springs: String,
-    num_unknown: usize,
-    checksum: Vec<u32>,
-}
-
-fn is_valid(springs: &str, checksum: &Vec<u32>) -> bool {
-    let counts = springs
-        .chars()
-        .group_by(|c| c == &'#')
-        .into_iter()
-        .filter_map(|(is_hashes, group)| is_hashes.then_some(group.into_iter().count() as u32))
-        .collect::<Vec<u32>>();
-    let valid = checksum == &counts;
-    if valid {
-        println!(
-            "springs {} is valid {} according to {:?}",
-            springs, valid, checksum
-        );
-    }
-    valid
-}
-
-fn unknowns_to_knowns(springs: &str, bits: &u64) -> String {
-    let mut unknown_index = 0;
-    springs
-        .chars()
-        .map(|ch| match ch {
-            '?' => {
-                let index = unknown_index;
-                unknown_index += 1;
-                if bits & (1 << index) != 0 {
-                    '.'
-                } else {
-                    '#'
-                }
-            }
-            _ => ch,
-        })
-        .collect()
-}
-
-impl Record {
-    fn is_valid_with_toggled_unknowns(&self, bits: &u64) -> bool {
-        let springs = unknowns_to_knowns(&self.springs, bits);
-        is_valid(&springs, &self.checksum)
-    }
+    checksum: Vec<usize>,
 }
 
 type Records = Vec<Record>;
@@ -65,39 +23,67 @@ fn parse_record(line: &str) -> Record {
     };
 
     let springs = std::iter::repeat(springs).take(5).join("?");
-    let num_unknown = springs.chars().filter(|c| c == &'?').count();
 
-    let checksum: Vec<u32> = checksum.split(",").map(|s| s.parse().unwrap()).collect();
-    let checksum: Vec<u32> = std::iter::repeat(checksum).take(5).flatten().collect();
-    println!("got final checksum {:?}, springs {:?}", checksum, springs);
+    let checksum: Vec<usize> = checksum.split(",").map(|s| s.parse().unwrap()).collect();
+    let checksum: Vec<usize> = std::iter::repeat(checksum).take(5).flatten().collect();
 
-    Record {
-        springs,
-        checksum,
-        num_unknown,
-    }
+    Record { springs, checksum }
 }
 
 fn parse_input(input: &str) -> Records {
     input.trim().lines().map(parse_record).collect()
 }
 
+fn count<'a>(cfg: &'a str, nums: &'a [usize], cache: &mut HashMap<(&'a str, &'a [usize]), usize>) -> usize {
+    if cfg.len() == 0 {
+        // println!("empty cfg {}, nums {:?}", cfg, nums);
+        return if nums.len() == 0 { 1 } else { 0 };
+    }
+
+    if nums.len() == 0 {
+        // println!("empty nums {:?}, cfg {}", nums, cfg);
+        return if cfg.contains('#') { 0 } else { 1 };
+    }
+
+    let key = (cfg, nums);
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap();
+    }
+
+    let mut result = 0;
+
+    // if we are at a working or potentially working spring...
+    let first_char = cfg.chars().next().unwrap();
+    if first_char == '.' || first_char == '?' {
+        // skip the spring and recurse with the same number of damaged numbers
+        // println!("get count for working");
+        result += count(&cfg[1..], nums, cache);
+    }
+
+    // if we are at a damaged or potentially damaged spring...
+    if first_char == '#' || first_char == '?' {
+        // and we have at least enough input left to satisfy the next damaged-group number
+        if nums[0] <= cfg.len()
+            // and the next nums[0] input doesn't contain any working springs
+            && !cfg[..nums[0]].contains('.') 
+            // and it's all the remaining input or there is something else beyond it (a working machine . or ?)
+            && (nums[0] == cfg.len() || cfg.chars().nth(nums[0]) != Some('#'))
+        {
+            // skip the length of the next damaged-group number and recurse without that number
+            let chop_from = (nums[0] + 1).min(cfg.len());
+            result += count(&cfg[chop_from..], &nums[1..], cache)
+        }
+    }
+
+    cache.insert(key, result);
+    result
+}
+
 fn get_answer(input: &str) -> usize {
     let records = parse_input(input);
     records
         .iter()
-        .enumerate()
-        .map(|(index, record)| {
-            let combinations = 2u64.pow(record.num_unknown as u32);
-            println!(
-                "counting valid of {} possible combinations for {} unknowns, record {}",
-                combinations, record.num_unknown, index,
-            );
-            (0..combinations)
-                .into_iter()
-                .filter(|bits| record.is_valid_with_toggled_unknowns(bits))
-                .count()
-        })
+        .map(|record|  count(&record.springs, &record.checksum, &mut HashMap::new()))
         .sum()
 }
 
